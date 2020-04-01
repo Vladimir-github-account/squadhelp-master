9851855ef48828e05b6b923bee7ec779724f2a1f
vladimir <s>
2020-04-01 16:07:04 +0300

    Refactor: add Logo component to form pages

--- client/src/pages/RegistrationPage/RegistrationPage.js
+++ client/src/pages/RegistrationPage/RegistrationPage.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
-import styles from './RegistrationPage.module.sass';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
+import React                      from 'react';
+import RegistrationForm           from '../../components/RegistrationForm/RegistrationForm';
+import styles                     from './RegistrationPage.module.sass';
+import {Link}                     from 'react-router-dom';
+import {connect}                  from 'react-redux';
 import {clearErrorSignUpAndLogin} from '../../actions/actionCreator';
-import CONSTANTS from '../../constants';
+import CONSTANTS                  from '../../constants';
+import Logo                       from '../../components/Logo';
 
 const RegistrationPage = (props) => {
     props.clearError();
@@ -17,7 +18,7 @@ const RegistrationPage = (props) => {
         <div className={styles.signUpPage}>
             <div className={styles.signUpContainer}>
                 <div className={styles.headerSignUpPage}>
-                    <img src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png`} alt="logo"/>
+                    <Logo src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png`}/>
                     <div className={styles.linkLoginContainer}>
                         <Link to='/login' style={{textDecoration: 'none'}}><span>Login</span></Link>
                     </div>
